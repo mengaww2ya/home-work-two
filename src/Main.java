01dd8dc65084699ada637b9a8aae74d65cf5e3a1
menga wubetu <mengaww2ya@gmail.com>
2023-10-29 02:35:43 -0700

    second homwrks

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // Create Student objects
-        Student student1 = new Student("MENGA", 1, new int[]{95, 89, 84});
-        Student student2 = new Student("MENGAW", 2, new int[]{54, 80, 90});
+        Student student1 = new Student("MENGAW WUBETU", 1, new int[]{95, 89, 84});
+        Student student2 = new Student("ALEMU KEBEDE", 2, new int[]{54, 80, 90});
 
         // Create Teacher objects
-        Teacher teacher1 = new Teacher("gash", "java");
-        Teacher teacher2 = new Teacher("memr", "database");
+        Teacher teacher1 = new Teacher("ANTENEH GUDETU", "java");
+        Teacher teacher2 = new Teacher("YETIMALE NIGUS", "database");
 
         // Create College object
         List<Student> students = new ArrayList<>();
@@ -20,25 +20,10 @@ public class Main {
         List<Teacher> teachers = new ArrayList<>();
         teachers.add(teacher1);
         teachers.add(teacher2);
-
-        College college = new College("ABC College", students, teachers);
+        College college = new College("COLLEGE OF COMPUTING AND INFORMATICS", students, teachers);
 
         // Print details
-        System.out.println("College Name: " + college.getName());
-
-        System.out.println("Students:");
-        for (Student student : college.getStudents()) {
-            System.out.println("Name: " + student.getName());
-            System.out.println("ID: " + student.getId());
-            System.out.println("Grades: " + Arrays.toString(student.getGrades()));
-            System.out.println();
-        }
-
-        System.out.println("Teachers:");
-        for (Teacher teacher : college.getTeachers()) {
-            System.out.println("Name: " + teacher.getName());
-            System.out.println("Subject: " + teacher.getSubject());
-            System.out.println();
+        
         }
     }
 }
